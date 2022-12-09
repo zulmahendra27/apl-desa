df9e296fe35b530da790924157c5b679ef35455c
zulmahendra27 <zulmahendra27@gmail.com>
2022-12-09 11:03:18 +0700

    Add user

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,24 +10,26 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="admin/assets/img/favicon.png" rel="icon">
+  <link href="admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  {{-- <link href="admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"> --}}
+  {{-- <link href="admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> --}}
+  {{-- <link href="admin/assets/vendor/quill/quill.snow.css" rel="stylesheet"> --}}
+  {{-- <link href="admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet"> --}}
+  {{-- <link href="admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet"> --}}
+  {{-- <link href="admin/assets/vendor/simple-datatables/style.css" rel="stylesheet"> --}}
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="admin/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.4.1
@@ -50,7 +52,7 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <span class="d-none d-lg-block">Aplikasi Desa</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -63,29 +65,23 @@
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
 
-                  <form class="row g-3 needs-validation" novalidate>
+                  <form class="row g-3 needs-validation" novalidate action="login" method="post">
 
                     <div class="col-12">
-                      <label for="yourUsername" class="form-label">Username</label>
+                      <label for="username" class="form-label">Username</label>
                       <div class="input-group has-validation">
                         <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" id="yourUsername" required>
+                        <input type="text" name="username" class="form-control" id="username" required>
                         <div class="invalid-feedback">Please enter your username.</div>
                       </div>
                     </div>
 
                     <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="yourPassword" required>
+                      <label for="password" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="password" required>
                       <div class="invalid-feedback">Please enter your password!</div>
                     </div>
 
-                    <div class="col-12">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-                        <label class="form-check-label" for="rememberMe">Remember me</label>
-                      </div>
-                    </div>
                     <div class="col-12">
                       <button class="btn btn-primary w-100" type="submit">Login</button>
                     </div>
@@ -114,17 +110,18 @@
     </div>
   </main><!-- End #main -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="admin/assets/vendor/chart.js/chart.min.js"></script>
-  <script src="admin/assets/vendor/echarts/echarts.min.js"></script>
-  <script src="admin/assets/vendor/quill/quill.min.js"></script>
-  <script src="admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="admin/assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="admin/assets/vendor/php-email-form/validate.js"></script>
+  {{-- <script src="admin/assets/vendor/apexcharts/apexcharts.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/chart.js/chart.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/echarts/echarts.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/quill/quill.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/simple-datatables/simple-datatables.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/tinymce/tinymce.min.js"></script> --}}
+  {{-- <script src="admin/assets/vendor/php-email-form/validate.js"></script> --}}
 
   <!-- Template Main JS File -->
   <script src="admin/assets/js/main.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/login', [LoginController::class, 'index']);
